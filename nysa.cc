c7b7dc4fdf465a76b1d1ad7d66d2cd484f62e9dc
Kapiszon2343 <46422065+Kapiszon2343@users.noreply.github.com>
2021-10-12 18:54:57 +0200

    Moja pełna część przed debugiem
    uzupełniłem output i dodałem funkcje pomocnicze output_step i calculate_single_output
    
    Zmieniłem typ gate aby funkcja w nim przyjmowała wskaźnik na mapę, a nie całą mapę. Funkcja AND została poprawiona aby odzwierciedlić tą zmianę

--- nysa.cc
+++ nysa.cc
@@ -4,7 +4,6 @@
 #include<vector>
 #include<string>
 #include<regex>
-#include<regex.h>
 #include<istream>
 #include<iostream>
 
@@ -23,16 +22,21 @@ using std::cerr;
 
 
 using signal_t = int32_t;
-using gate = pair< void (*)(pair<signal_t, vector<signal_t> >, unordered_map<signal_t, bool>), pair<signal_t, vector<signal_t> > >;
+using states_map = unordered_map<signal_t, bool>;
+using gate = pair< 
+	void (*)(pair<signal_t, vector<signal_t> >, states_map*), 
+	pair<signal_t, vector<signal_t> > 
+	>;
+
 
 namespace  {
 	void AND_f(pair<signal_t, vector<signal_t> > out_in, 
-		unordered_map<signal_t, bool> states) {
+		states_map *states) {
 		bool output_state = true;
 		for(auto in_id : out_in.second) {
-			output_state &= states[in_id];
+			output_state &= (*states)[in_id];
 		}
-		states[out_in.first] = output_state;
+		(*states)[out_in.first] = output_state;
 	}
 	
 	void read_input(vector<gate> const *gates) {
@@ -89,7 +93,7 @@ namespace  {
 		
 		queue<signal_t> ready_inputs;
 		
-		for ( auto it = in_out.begin(); it != in_out.end(); ++it  ) {
+		for (auto it = in_out.begin(); it != in_out.end(); ++it  ) {
 			if(it->second == 1) {
 				ready_inputs.push(it->first);
 			}
@@ -119,8 +123,109 @@ namespace  {
 		return sequential_logic;
 	}
 	
+	void calculate_single_output(
+		vector<gate> const *gates,
+		unordered_map<signal_t, vector<size_t> > *gates_per_input,
+		vector<signal_t> const *inputs,
+		vector<signal_t> const *outputs, 
+		states_map *states) {
+		
+		queue<size_t> ready_inputs;
+		vector<int> waiting_for_inputs;
+		for(size_t i = 0; i < gates->size(); ++i) {
+			waiting_for_inputs.push_back((*gates)[i].second.second.size());
+		}
+		for(signal_t input : *inputs) {
+			ready_inputs.push(input);
+		}
+		
+		
+		while(!ready_inputs.empty()) {
+			signal_t input = ready_inputs.front();
+			ready_inputs.pop();
+			
+			for(size_t i : (*gates_per_input)[input]) {
+				--waiting_for_inputs[i];
+				if(waiting_for_inputs[i] == 0) {
+					(*gates)[i].first((*gates)[i].second, states);
+					ready_inputs.push((*gates)[i].second.first);
+				}
+			}
+		}
+
+		for(signal_t output : *outputs) {
+			printf("%d", (*states)[output]);
+		}
+		printf("\n");
+	}
+	
+	void output_step(
+		vector<gate> const *gates,
+		unordered_map<signal_t, vector<size_t> > *gates_per_input,
+		vector<signal_t> const *inputs, 
+		vector<signal_t> const *outputs, 
+		states_map *states, 
+		size_t done_steps) {
+			if(done_steps == inputs->size()) {
+				calculate_single_output(
+					gates,
+					gates_per_input, 
+					inputs, 
+					outputs, 
+					states);
+			}
+			else {
+				(*states)[(*inputs)[done_steps]] = 0;
+				output_step(
+					gates,
+					gates_per_input, 
+					inputs, 
+					outputs, 
+					states, 
+					done_steps+1);
+				(*states)[(*inputs)[done_steps]] = 1;
+				output_step(
+					gates,
+					gates_per_input, 
+					inputs, 
+					outputs, 
+					states, 
+					done_steps+1);
+			}
+	}
+	
 	void output(vector<gate> const *gates) {
-		//TODO
+		vector<int> waiting_for_inputs;
+		unordered_map<signal_t, vector<size_t> > gates_per_input;
+		unordered_map<signal_t, int> in_out; // bit 1 lit -> is an input; bit 2 lit -> is an output
+		vector<signal_t> only_inputs, only_outputs;
+		
+		for(size_t i = 0; i < gates->size(); ++i) {
+			waiting_for_inputs.push_back((*gates)[i].second.second.size());
+			in_out[(*gates)[i].second.first] |= 2;
+			for(signal_t input : (*gates)[i].second.second) {
+				gates_per_input[input].push_back(i);
+				in_out[input] |= 1;
+			}
+		}
+		
+		for ( auto it = in_out.begin(); it != in_out.end(); ++it  ) {
+			if(it->second == 1) {
+				only_inputs.push_back(it->first);
+			}
+			if(it->second == 2) {
+				only_outputs.push_back(it->first);
+			}
+		}
+		
+		states_map states;
+		output_step(
+			gates,
+			&gates_per_input, 
+			&only_inputs, 
+			&only_outputs, 
+			&states, 
+			0);
 	}
 }
 
